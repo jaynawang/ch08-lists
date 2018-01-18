ae68142f37811457dc23e30a8cbbb7384641fb8e
Jayna <jaynawang@D-69-91-141-81.dhcp4.washington.edu>
2018-01-17 16:57:29 -0800

    change exercise with my answers to add

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,29 +2,34 @@
 
 # 1. Create a vector of everything you ate for breakfast
 
+breakfast <- list("cereal", "milk")
 
 # 2. Create a vector of everything you ate for lunch
 
-
+lunch <- list("granola bar", "bread", "egg", "cilantro")
 ## 3. Create a list "meals" that has contains your `breakfast` and `lunch`
 ## Ensure the components of your list are named accordingly
 
+meals <- list(breakfast, lunch)
 
 ## 4. Add a "dinner" element to your "meals" list that has what you plan to eat for dinner.
 ## you may use assignment with dollar or double brackets.
 
+meals$dinner <- list("omelette", "red bell pepper", "mushrooms", "cilantro")
 
 # 5. Use dollar notation to extract your `dinner` element from your list
 # and save it in a vector called 'dinner'
 
+dinner <- meals$dinner
 
 # 6. Use double-bracket notation to extract your `lunch` element from your list
 
+lunch <- meals[[2]]
 
 # 7. Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early.meals`
 
-
+early.meals <- meals[c(1, 2)]
 
 ### Bonus ###
 # 8. Create a list that has the number of items you ate for each meal
